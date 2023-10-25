3cd6b23b73c2d741c8a1738b0ff4e762cfa99aeb
RudrakshaSingh <rudrakshasingh587@gmail.com>
2023-10-25 07:02:57 -0700

    assests added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import Footer from "./Components/Footer";
+import HomePage from "./Pages/HomePage";
 
 function App() {
-    return <>{/* <Routes><Route path="/" element={<HOME/>}><Route/></Routes> */}</>;
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
