a44071306b7a0b0bb19d0cc794b055a34f619c5f
Thakur Rudraksha Singh <rudrakshasingh587@gmail.com>
2025-04-22 10:14:16 +0530

    cors 4

--- server/app.js
+++ server/app.js
@@ -17,20 +17,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //for params to parse encoded url
 
 app.use(
-    cors({
-        origin: [process.env.FRONTEND_URL], //to use api in different user
-        credentials: true, // to let cookie or credentials trvel through
-    })
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
 );
-
 
 app.use(cookieParser()); //to let token in cookies parse
 
 app.use(morgan("dev")); // dev level ki information console pe print karna
 
 app.use("/ping", (req, res) => {
-    //to check if server is running
-    res.send("Pong");
+  //to check if server is running
+  res.send("Pong");
 });
 
 //routes in 3 modules
@@ -40,8 +39,8 @@ app.use("/api/v1/payments", paymentRoutes);
 app.use("/api/v1", miscRoutes);
 
 app.all("*", (req, res) => {
-    //if any random url is seraches expect defined ones
-    res.status(404).send("OOPS!! 404 page not fount");
+  //if any random url is seraches expect defined ones
+  res.status(404).send("OOPS!! 404 page not fount");
 });
 
 app.use(errorMiddleware); //after defining your routes and setting up other middleware
